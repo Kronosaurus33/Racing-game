f2f64ded270c41113b85f91e9563276151a6ccad
JD Thompson <jd.thomp@yahoo.com>
2017-06-16 18:06:56 -0400

    Window Update
    WINDOWS!!!

---
+++ AI_Tutorial (4)/AI_Tutorial/Assets/Scripts/windowBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windowBehaviour : MonoBehaviour {
+
+		void OnCollisionEnter (Collision col)
+		{
+				if(col.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+				{
+						this.gameObject.GetComponent<Animator> ().SetBool ("ColidedPlayer", true);
+				}
+		}
+}
